6fd763d46a44930f40a8f420dddea158d8f64d7f
apaparun <36165446+apaparun@users.noreply.github.com>
2018-09-02 23:59:57 +0530

    Update oddeven.py

--- beginer/oddeven.py
+++ beginer/oddeven.py
@@ -1,7 +1,7 @@
-num=int(raw_input("Enter the number: "))
-if((num%2)==0):
+num=float(raw_input("Enter the number: "))
+if(num%2==0):
     print("Number is even")
-elif((num%2)!=0):
+elif(num%2==1):
     print("Number is odd")
 else:
     print("Invalid input")

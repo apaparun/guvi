6d191f46bcdce649ae6e3514b50683178268e5f0
ArunPrasanth A P <36165446+apaparun@users.noreply.github.com>
2018-09-16 11:25:59 +0530

    Update armstrong_num.py

--- beginer/armstrong_num.py
+++ beginer/armstrong_num.py
@@ -1,12 +1,14 @@
 n=int(input("Enter the number: "))
 sum=0
 temp=n
-while temp>0:
-    dig=temp%10
-    sum+=dig**3
-    temp//=10
-if sum==n:
-    print n,"is armsrtong number"
+if n<=100000:
+    while temp>0:
+        dig=temp%10
+        sum+=dig**3
+        temp//=10
+    if sum==n:
+        print n,"is armsrtong number"
+    else:
+        print n,"is not an armstrong number"
 else:
-    print n,"is not an armstrong number"
-    
+    print "Enter number less than 100000"

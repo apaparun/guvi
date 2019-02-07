1ba708e087e67542ec374b0c02c1f213df8a89b0
ArunPrasanth A P <36165446+apaparun@users.noreply.github.com>
2019-02-07 11:02:33 +0530

    Update factorial.py

--- beginer/factorial.py
+++ beginer/factorial.py
@@ -1,5 +1,8 @@
 n=int(raw_input())
 facto=1
-for i in range(1,n+1):
-  facto=facto*n
-print(facto)
+if (n<0):
+	print("factorial does not exist")
+else:
+	for i in range(1,n+1):
+		facto=facto*i
+	print (facto)

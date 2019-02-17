ea99e34e4b01b86cdad26a1a2d4332b07544eb71
ArunPrasanth A P <36165446+apaparun@users.noreply.github.com>
2019-02-17 23:37:46 +0530

    Update remove_reverse_vowel.py

--- player/remove_reverse_vowel.py
+++ player/remove_reverse_vowel.py
@@ -1,7 +1,11 @@
+n=int(raw_input())
 string=raw_input()
 vowels = ('a', 'e', 'i', 'o', 'u')
-for x in string.lower():
-	if x in vowels:
-		string = string.replace(x, "")
-string = string[::-1]
-print string
+if len(string)==n and 1<=len(string)<=1000000 :
+	for x in string.lower():
+		if x in vowels:
+			string = string.replace(x, "")
+	string = string[::-1]
+	print string
+else:
+	print "invalid"

f74bea27a6f52377a2fb2844c29ec6d83179219b
ArunPrasanth A P <36165446+apaparun@users.noreply.github.com>
2019-02-17 20:44:26 +0530

    Update odd_position_even_position.py

--- hunter/set1/odd_position_even_position.py
+++ hunter/set1/odd_position_even_position.py
@@ -1,17 +1,17 @@
 while True:
-	try:
-		n=int(raw_input())
-		num=[]
-		for i in range(n):
-			num= list(map(int, raw_input().split()))
-		break
-	except:
-		break
+    try:
+        n=int(raw_input())
+	num=[]
+	for i in range(n):
+            num= list(map(int, raw_input().split()))
+        break
+    except:
+        break
 odd,eve=[],[]
 for i in range(n):
-    if i%3==0 and num[i]%3!=0:
+    if i%2==0 and num[i]%2!=0:
         odd.append(num[i])
-    elif i%3!=0 and num[i]%3==0:
+    elif i%2!=0 and num[i]%2==0:
         odd.append(num[i])
 for k in odd:
 	print k,

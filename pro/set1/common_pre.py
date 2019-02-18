6a61c737ddee434dc6c8b96b122fa7d2664feea8
ArunPrasanth A P <36165446+apaparun@users.noreply.github.com>
2019-02-18 19:52:29 +0530

    Create common_pre.py

---
+++ pro/set1/common_pre.py
@@ -0,0 +1,17 @@
+def ComPrefix( a): 
+    size = len(a)  
+    if (size == 0): 
+        return ""
+    if (size == 1): 
+        return a[0] 
+    a.sort()
+    end = min(len(a[0]), len(a[size - 1]))  
+    i = 0
+    while (i < end and a[0][i] == a[size - 1][i]): 
+        i += 1
+    pref = a[0][0: i] 
+    return pref
+if __name__ == "__main__": 
+    t = int(input())
+    arr = list(map(str, input().split())) 
+    print(ComPrefix(arr)) 
